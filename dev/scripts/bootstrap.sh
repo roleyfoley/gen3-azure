568c382f96cc0c7e691c5036d19af020eb104642
Ross Murray <ross.murray@protonmail.com>
2019-07-19 12:24:05 +1000

    Development (#4)
    proof of concept for using a bootstrap script after infrastructure deployment within azure.

---
+++ dev/scripts/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# This script is intended for use as a proof-of-concept for ARM template bootstrap scripts only.
+
+# Install Az CLI
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Create a file to copy to the Blob Storage
+touch ~/myfile.txt
+
+# Login to Azure with the CLI
+az login --identity
+
+# Upload the new file to Blob Storage to verify that the system-managed idenity is correct.
+az storage blob upload --container-name blobcontainer --file ~/myfile.txt --name "blobby07" --account-name "storage$1"
